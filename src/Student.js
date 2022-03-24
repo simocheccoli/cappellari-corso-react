7bad46d73c9bc1aa4ee554ff2c112c77b7aef1af
Simone Checcoli <8978038+simocheccoli@users.noreply.github.com>
2022-03-24 10:06:51 +0100

    Update Student.js

--- src/Student.js
+++ src/Student.js
@@ -6,29 +6,9 @@ class Student extends Component {
 
         console.log(this.props)
         //proprietà
-        /*
-        const {
-            name,
-            surname,
-            age,
-            experienceLists,
-        } = this.props
-*/
         const name = this.props.name;
         const surname = this.props.surname;
-       const voti = this.props.voti;
-
-        /*const voto1 = this.props.voto1;
-        const voto2 = this.props.voto2;
-        const voto3 = this.props.voto3;*/
-
-        /*voti = [
-           {
-               voto1: 7,
-               voto2: 8,    
-               voto3: 7,
-           },
-       ]*/
+        const voti = this.props.voti;
 
         //interfaccia
         return (
@@ -44,13 +24,6 @@ class Student extends Component {
         let voti = (voti[0] += voti[1] += voti[2])/3;
         return voti;
     }
-
-  /*  calcola_media(){
-        let media = (voto1+voto2+voto3)/3; //non definiti
-
-        return media;
-
-    }*/
 }
 
 export default Student
