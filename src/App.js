1a9c48b13e82eb4cca571cc04d4ee6f905e41c2d
Antonio Ciaccia <antoio_ciaccia@libero.it>
2022-03-24 17:15:31 +0100

    Code refactoring & scaffolding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Student from "./Student";
+import Student from "./Components/Student/Student.js";
 
 function App() {
-
-    const navbar = <div className="container">
-        <h1 className="site-heading">Hello, React</h1>
-    </div>
-
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
 
-                <Student surname={'Ciaccia'} name={'Antonio'} voti={[2,9,8,7,10]}/>
             </header>
 
 
--- src/Components/Student/Student.js
+++ src/Components/Student/Student.js
@@ -4,25 +4,9 @@ import React, {Component} from 'react'
 //definizione del componente
 class Student extends Component {
     render() {
-
-        // console.log(this.props)
-        //proprietà
-        /*
-        const {
-            name,
-            surname,
-            age,
-            experienceLists,
-        } = this.props
-*/
         const name = this.props.name;
         const surname = this.props.surname;
-        // const voto1 = this.props.voto1;
-        // const voto2 = this.props.voto2;
-        // const voto3 = this.props.voto3;
-        // const voti = this.props.voti;
 
-        //interfaccia
         return (
             <div>
                 <p>Nome : {name}</p>
@@ -34,10 +18,13 @@ class Student extends Component {
 
     calcolaMedia() {
         const voti = this.props.voti;
-        const sommaVoti = voti.reduce((prevValue, curValue) => prevValue + curValue);
-        let media = sommaVoti / voti.length;
-
-        return media;
+        if (voti && voti.length > 0) {
+            const sommaVoti = voti.reduce((prevValue, curValue) => prevValue + curValue);
+            let media = sommaVoti / voti.length;
+            return media;
+        } else {
+            return '-'
+        }
     }
 }
 
