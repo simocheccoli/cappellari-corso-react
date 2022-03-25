116ede5bb5df5ff311ed15ae5930225dc57ac7c0
Antonio Ciaccia <antoio_ciaccia@libero.it>
2022-03-25 15:01:41 +0100

    aggiunta pagina Home

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Routes, Route, Link} from "react-router-dom";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
