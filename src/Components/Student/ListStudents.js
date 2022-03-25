1979053adc9e7dae0985e2fdab254eba1cca5f5b
Antonio Ciaccia <antoio_ciaccia@libero.it>
2022-03-25 15:04:32 +0100

    aggiunto ListStudent component

---
+++ src/Components/Student/ListStudents.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {getStudents} from "../data/data";
+import {Link, Outlet} from "react-router-dom";
+
+class ListStudents extends Component {
+    render() {
+        let students = getStudents();
+        return (
+            <div>
+                <h1>Lista studenti</h1>
+                <nav>
+                    {students.map((student) => (
+                        <Link
+                            style={{ display: "block", margin: "1rem 0" }}
+                            to={`/students/${student.id}`}
+                            key={student.id}
+                        >
+                            {student.name}
+                        </Link>
+                    ))}
+                </nav>
+                <Outlet/>
+            </div>
+        )
+    }
+}
+
+export default ListStudents;
