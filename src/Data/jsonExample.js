e75c7744f28aa8253bdbc8b775453c083925402c
jaska777 <nicola.donega@outlook.it>
2022-04-06 17:35:02 +0200

    update example

--- src/Data/jsonExample.js
+++ src/Data/jsonExample.js
@@ -1,7 +1,15 @@
 const data = [
-    {
+    componentA = {
         address: 'via xxx',
         tel: 0000000000,
         mail: 'info@info.it'
+    },
+    componentB = {
+        name: 'nome',
+        surname: 'cognome',
+        job: 'dev'
+    },
+    componentC = {
+        //img
     }
 ]
