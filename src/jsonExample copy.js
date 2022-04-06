b6ef081a5b697c04a4308886b3469a1444e09850
jaska777 <nicola.donega@outlook.it>
2022-04-06 19:10:32 +0200

    fabio for you

---
+++ src/jsonExample copy.js
@@ -0,0 +1,55 @@
+const data = [
+    Contacts = {
+        address: 'via xxx',
+        tel: 0000000000,
+        mail: 'info@info.it'
+    },
+    User = {
+        name: 'nome',
+        surname: 'cognome',
+        job: 'dev'
+    },
+    Photo = {
+        //img
+    },
+    About = { description:''
+        
+    },
+
+    Education = [
+        {
+            from: '',
+            to: '',
+            institute: '',
+            title: ''
+        }
+        
+    ],
+
+    Skills = [{type:''}],
+
+    Software = [{type:''}],
+
+    Reference = [
+        {
+            name: '',
+            surname: '',
+            number: '',
+            email:''
+        }
+    ],
+
+    Experience = [
+        {
+            from:'',
+            to:'',
+            job:'',
+            company:'',
+            description:''
+        }
+    ]
+
+
+
+
+]
