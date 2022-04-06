32ca9143d860cd037fe3b2534e0d60d922f00f82
FabioMezzo <mezzogorifabio006@gmail.com>
2022-04-06 22:29:38 +0200

    avanzamento componente contacts

---
+++ src/Contacts/Contacts.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import data from './data/Contacts.js'
+
+class Contacts extends Component{
+
+render(){
+    const address = data.Contacts.address;
+    const number = data.Contacts.number;
+    const mail = data.Contacts.mail;
+    console.log(data.Contacts.address);
+
+
+return 1
+
+}
+
+
+}
+
+export default Contacts
